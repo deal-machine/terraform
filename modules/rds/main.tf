911bd180d5682bb97f0ef43b91843ad3b71a89e4
deal-machine <95deal@gmail.com>
2024-02-20 01:46:27 -0300

    feat: add configs in rds

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -5,12 +5,22 @@ resource "aws_db_subnet_group" "subnet" {
         Name = "${var.prefix}-group"
     }
 }
+resource "aws_db_parameter_group" "family" {
+  name   = "${var.prefix}-family"
+  family = "postgres14"
+
+  parameter {
+    name  = "log_connections"
+    value = "1"
+  }
+}
 resource "aws_db_instance" "rds" {
     instance_class = "db.t3.micro"
     allocated_storage = 5
     max_allocated_storage = 10
     storage_type = "gp2"
     publicly_accessible = true
+    skip_final_snapshot = true
 
     engine = "postgres"
     engine_version= "14.1"
@@ -20,6 +30,7 @@ resource "aws_db_instance" "rds" {
     
     db_subnet_group_name = aws_db_subnet_group.subnet.name
     vpc_security_group_ids = var.sg_ids
+    parameter_group_name = aws_db_parameter_group.family.name
 
     tags = {
         Name = "${var.prefix}-rds"
